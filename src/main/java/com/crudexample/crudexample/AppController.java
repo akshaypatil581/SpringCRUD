5b6edfa89401c910688bf71dcd405ced96016d55
akshaypatil581 <akshaypatil581@gmail.com>
2020-07-20 10:48:06 -0700

    Adding edit product page

--- src/main/java/com/crudexample/crudexample/AppController.java
+++ src/main/java/com/crudexample/crudexample/AppController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -27,6 +29,14 @@ public class AppController {
         Product product = new Product();
         model.addAttribute("product", product);
         return "new_product";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public ModelAndView showEditProductFrom(@PathVariable(name = "id") Long id) {
+        ModelAndView mav = new ModelAndView("edit_product");
+        Product product = service.get(id);
+        mav.addObject("product", product);
+        return mav;
     }
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
